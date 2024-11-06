310e163b51bb98d637fba8e6bc06f79512a79661
ShadowjGames <98559091+ShadowjGames@users.noreply.github.com>
2024-11-05 23:35:34 -0500

    Update waves.js

--- waves.js
+++ waves.js
@@ -1,80 +1,46 @@
-const backgroundSketch = (p) => {
-  let circles = [];
+const waveSketch = (p) => {
+  let yValues = [];
+  let theta = 0;
+  let waveAmplitude = 75;
+  let waveFrequency = 0.05;
   let explode = false;
-  let centerX, centerY;
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    centerX = p.width / 2;
-    centerY = p.height / 2;
-    for (let i = 0; i < 50; i++) {
-      circles.push(new FloatingCircle(p.random(p.width), p.random(p.height), p));
+    for (let x = 0; x < p.width; x += 10) {
+      yValues.push(0);
     }
   };
 
   p.draw = () => {
     p.clear();
-    for (let circle of circles) {
-      circle.update();
-      circle.display();
+
+    if (!explode) {
+      // Dibuja las ondas normales
+      p.noFill();
+      p.stroke(0, 100, 200, 150);
+      p.strokeWeight(2);
+      theta += 0.02;
+      let x = theta;
+      for (let i = 0; i < yValues.length; i++) {
+        yValues[i] = Math.sin(x) * waveAmplitude;
+        x += waveFrequency;
+      }
+      for (let x = 0; x < yValues.length; x++) {
+        p.ellipse(x * 10, p.height / 2 + yValues[x], 10, 10);
+      }
     }
   };
 
   p.mousePressed = () => {
     if (p.mouseButton === p.LEFT) {
       explode = true;
-      for (let circle of circles) {
-        circle.moveToCenter(centerX, centerY);
-      }
     }
   };
-
-  p.mouseReleased = () => {
-    explode = false;
-    circles = [];
-    // Genera nuevos círculos dispersos
-    for (let i = 0; i < 50; i++) {
-      circles.push(new FloatingCircle(p.random(p.width), p.random(p.height), p));
-    }
-  };
-
-  class FloatingCircle {
-    constructor(x, y, p) {
-      this.p = p;
-      this.pos = this.p.createVector(x, y);
-      this.vel = p5.Vector.random2D().mult(this.p.random(1, 3));
-      this.target = this.p.createVector(centerX, centerY);  // Centro para la explosión
-      this.size = this.p.random(20, 50);
-      this.opacity = this.p.random(50, 100);
-    }
-
-    update() {
-      if (explode) {
-        // Los círculos se acercan al centro
-        let force = p5.Vector.sub(this.target, this.pos).mult(0.03);
-        this.vel.add(force);
-        this.vel.limit(8);
-      }
-      this.pos.add(this.vel);
-    }
-
-    moveToCenter(cx, cy) {
-      // Establece el centro como el objetivo
-      this.target.set(cx, cy);
-    }
-
-    display() {
-      this.p.noStroke();
-      this.p.fill(200, this.opacity);  // Círculo gris semitransparente
-      this.p.ellipse(this.pos.x, this.pos.y, this.size);
-    }
-  }
 
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
-    centerX = p.width / 2;
-    centerY = p.height / 2;
   };
 };
 
-new p5(backgroundSketch, "wave-canvas");
+new p5(waveSketch, "wave-canvas");
