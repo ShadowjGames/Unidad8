a023a93bb2a3ebaaf67933dc8b7359024e89f95b
ShadowjGames <98559091+ShadowjGames@users.noreply.github.com>
2024-11-05 22:20:03 -0500

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,94 +1,83 @@
 // particles.js
 
-class Particle {
-  constructor(x, y) {
-    this.position = createVector(x, y);
-    this.velocity = createVector(random(-2, 2), random(-2, 0));
-    this.acceleration = createVector(0, 0.07);
-    this.lifespan = 255;
-    this.cr = random(255);
-    this.cg = random(255);
-    this.cb = random(255);
-  }
-
-  applyForce(force) {
-    let f = force.copy();
-    f.div(1); // Masa de 1
-    this.acceleration.add(f);
-  }
-
-  update() {
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.acceleration.mult(0);
-
-    // Rebote en los bordes
-    if (this.position.y > height) {
-      this.velocity.y *= -0.8; // Reducción de velocidad al rebotar
-      this.position.y = height;
+const particleSketch = (p) => {
+  const symbols = ['X', '□', '○', '△'];
+  
+  class Particle {
+    constructor(x, y) {
+      this.position = p.createVector(x, y);
+      this.velocity = p5.Vector.random2D().mult(p.random(1, 3)); // Movimiento en direcciones aleatorias
+      this.acceleration = p.createVector(0, 0);
+      this.lifespan = 255;
+      this.symbol = p.random(symbols); // Símbolo aleatorio de PlayStation
+      this.color = p.color(p.random(255), p.random(255), p.random(255));
     }
-    
-    this.lifespan -= 2;
-  }
 
-  display() {
-    stroke(255, this.lifespan);
-    strokeWeight(2);
-    fill(this.cr, this.cg, this.cb, this.lifespan);
-    ellipse(this.position.x, this.position.y, 12, 12);
-  }
+    applyForce(force) {
+      let f = force.copy();
+      f.div(1);
+      this.acceleration.add(f);
+    }
 
-  isDead() {
-    return this.lifespan <= 0;
-  }
-}
+    update() {
+      this.velocity.add(this.acceleration);
+      this.position.add(this.velocity);
+      this.acceleration.mult(0);
+      this.lifespan -= 2;
+    }
 
-class ParticleSystem {
-  constructor(x, y) {
-    this.origin = createVector(x, y);
-    this.particles = [];
-  }
+    display() {
+      p.fill(this.color.levels[0], this.color.levels[1], this.color.levels[2], this.lifespan);
+      p.noStroke();
+      p.textSize(20);
+      p.textAlign(p.CENTER, p.CENTER);
+      p.text(this.symbol, this.position.x, this.position.y);
+    }
 
-  addParticle() {
-    this.particles.push(new Particle(this.origin.x, this.origin.y));
-  }
-
-  applyForce(force) {
-    for (let particle of this.particles) {
-      particle.applyForce(force);
+    isDead() {
+      return this.lifespan <= 0;
     }
   }
 
-  run() {
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      let particle = this.particles[i];
-      particle.update();
-      particle.display();
-      if (particle.isDead()) {
-        this.particles.splice(i, 1);
+  class ParticleSystem {
+    constructor(x, y) {
+      this.origin = p.createVector(x, y);
+      this.particles = [];
+    }
+
+    addParticle() {
+      this.particles.push(new Particle(this.origin.x, this.origin.y));
+    }
+
+    run() {
+      for (let i = this.particles.length - 1; i >= 0; i--) {
+        let particle = this.particles[i];
+        particle.update();
+        particle.display();
+        if (particle.isDead()) {
+          this.particles.splice(i, 1);
+        }
       }
     }
   }
-}
 
-let ps;
+  let ps;
 
-function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("canvas-container");
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    ps = new ParticleSystem(p.width / 2, p.height / 2); // Centro de la pantalla
+  };
 
-  ps = new ParticleSystem(width / 2, height / 2);
-}
+  p.draw = () => {
+    p.clear();
+    ps.addParticle();
+    ps.run();
+  };
 
-function draw() {
-  clear(); // Limpia la pantalla en cada cuadro para la transparencia
-  let gravity = createVector(0, 0.2);
-  ps.applyForce(gravity);
-  ps.addParticle();
-  ps.run();
-}
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+};
 
-// Ajuste de tamaño de canvas al cambiar el tamaño de la ventana
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+// Crear instancia de p5 para las partículas
+new p5(particleSketch, "canvas-container");
